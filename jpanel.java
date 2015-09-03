227295a23ca974d7a9a868f94c84aae2975acdd6
Capa8.tv <canalcapa8@gmail.com>
2015-09-02 23:14:56 -0400

    jpanel.java

---
+++ jpanel.java
@@ -0,0 +1,13 @@
+
+package tallerjava3;
+
+import javax.swing.JOptionPane;
+
+
+public class jpanel {
+     public static void main(String[] args) 
+     {
+        String nombre =JOptionPane.showInputDialog("Introduce tu nombre");
+         System.out.println("su nombre es = "+nombre);
+     }
+}
