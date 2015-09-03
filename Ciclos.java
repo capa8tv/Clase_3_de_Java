d20fa50b34f47ed9c0755e4f90038c3b361c4be6
Capa8.tv <canalcapa8@gmail.com>
2015-09-02 23:14:35 -0400

    Ciclos.java

---
+++ Ciclos.java
@@ -0,0 +1,23 @@
+
+package tallerjava3;
+
+public class Ciclos 
+{
+    public static void main(String[] args) 
+    {
+        //ciclo for
+        //cada vuelta que da un ciclo se llama Iteración
+        // buble
+        for( int i=1 ; i <= 10 ; i++ )
+        {
+            if(i%2==0)//si el valor de i es par
+            {
+                System.out.println("el contador es = " + i ); 
+            }  
+           
+        }
+        
+      
+      
+    }
+}
